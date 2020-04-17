1da68e34a80289b3ceea6766a809781642fefc31
Alec Marcus <alec@alecmarc.us>
2020-04-17 18:35:59 -0400

    demo

---
+++ pages/index.vue
@@ -0,0 +1,89 @@
+<template>
+  <main>
+    <header class="main-header">
+      <h1 class="light-heading">Upstatement Public Library</h1>
+      <!-- The "pre" tag denotes code. Browsers will used the default monospaced font when rendering them. -->
+      <pre>$ find _</pre>
+    </header>
+    <section>
+      <header class="sub-header">
+        <h2 class="light-heading">/Learn/Lessons</h2>
+        <p class="page-intro">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </header>
+      <section>
+        <h3 class="section-heading">/Project Setup</h3>
+        <ul class="lesson-list">
+          <li class="lesson-list-item">
+            <h4 class="lesson-heading">
+              <a href="/">Intro to Terminal</a>
+            </h4>
+            <div class="lesson-desc">
+              <p>
+                Learn what the terminal is on your Mac, and how to use it to
+                control and navigate your computer.
+              </p>
+              <ul>
+                <li>Identifying your shell</li>
+                <li>Issuing sh commands in terminal</li>
+                <li>Navigating the filesystem and constructing file paths</li>
+                <li>Creating and opening files in terminal</li>
+              </ul>
+            </div>
+          </li>
+          <li class="lesson-list-item">
+            <h4 class="lesson-heading">
+              <a href="/">Intro to Git</a>
+            </h4>
+            <div class="lesson-desc">
+              <p>
+                Learn what git and GitHub are and how to use them to collaborate
+                on Upstatement projects.
+              </p>
+              <ul>
+                <li>
+                  Git basics (branches, commits,
+                  <a href="/">push</a>,
+                  <!-- this comment is here to ensure extra whitespace doesn't get added between the comma and the next link -->
+                  <a href="/">pull</a>, merge)
+                </li>
+                <li>Learning and using core git commands</li>
+                <li>Creating branches and commits</li>
+                <li>Writing properly worded commit messages</li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h3 class="section-heading">/HTML</h3>
+        <ul class="lesson-list">
+          <li class="lesson-list-item">
+            <h4 class="lesson-heading">
+              <a href="/">Intro to HTML</a>
+            </h4>
+            <div class="lesson-desc">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+          </li>
+          <li class="lesson-list-item">
+            <h4 class="lesson-heading">
+              <a href="/">HTML Elements</a>
+            </h4>
+            <div class="lesson-desc">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </section>
+    </section>
+  </main>
+</template>
